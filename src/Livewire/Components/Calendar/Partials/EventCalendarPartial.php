fbebed087899e3c1992e470705addf7939ba8fdb
Josh Torres <josht@omniadigital.io>
2023-12-23 20:59:20 -0800

    - add Google Calendar spatie package so I can pull in Google Calendar events into the calendar

--- src/Livewire/Components/Calendar/Partials/EventCalendarPartial.php
+++ src/Livewire/Components/Calendar/Partials/EventCalendarPartial.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Support\Collection;
 use Omnia\LivewireCalendar\LivewireCalendar;
 use OmniaDigital\CatalystCore\Models\Event;
+use Spatie\GoogleCalendar\Event as GoogleCalendarEvent;
 
 class EventCalendarPartial extends LivewireCalendar
 {
@@ -19,11 +20,21 @@ class EventCalendarPartial extends LivewireCalendar
 
     public function events(): Collection
     {
+        return GoogleCalendarEvent::get()->map(function (GoogleCalendarEvent $event) {
+            return [
+                'id' => $event->id,
+                'title' => $event->name,
+                'description' => $event->summary,
+                'date' => $event->starts_at,
+                'count' => 1,
+                'location' => $event->location,
+            ];
+        });
         return Event::query()->publicAndPublished()
             ->whereDate('starts_at', '>=', $this->gridStartsAt)
             ->whereDate('starts_at', '<=', $this->gridEndsAt)
             ->get()
-            ->map(function (Event $event) {
+            ->map(function (GoogleCalendarEvent $event) {
                 return [
                     'id' => $event->id,
                     'title' => $event->name,
@@ -37,7 +48,7 @@ class EventCalendarPartial extends LivewireCalendar
     public function goToMonth($eventID)
     {
         $this->selectedID = $eventID;
-        $date = Event::find($eventID)->start_date;
+        $date = GoogleCalendarEvent::find($eventID)->start_date;
 
         $this->startsAt = $date->startOfMonth()->startOfDay();
         $this->endsAt = $this->startsAt->clone()->endOfMonth()->startOfDay();
